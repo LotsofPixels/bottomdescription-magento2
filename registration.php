46a44f854fa09cc4e5606877bfd407680fe6c984
Rene Donkers <rene@lotsofpixels.io>
2023-07-30 17:28:11 +0000

    Intial release 1.0.0

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'LotsofPixels_Bottomdescription',
+    __DIR__
+);
